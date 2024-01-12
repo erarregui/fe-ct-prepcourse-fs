0da2bda337e32792005730eb0dde861ce1ebab53
er.arregui <er.arregui@gmail.com>
2024-01-12 10:36:14 -0300

    Homework html - ejerccios MO5 1 al 7

--- M05 Fundamentos Javascript/ejercicios/08.js
+++ M05 Fundamentos Javascript/ejercicios/08.js
@@ -3,6 +3,13 @@ function esNumeroEntero(numero) {
   // Verifica si este es un número entero o no.
   // Returna true si lo es, de lo contrario, retorna false.
   // Tu código:
+  if (Number.isInteger(numero) === true){
+    return true
+  }else{
+    return false
+
+    }
+  
 }
 
 module.exports = esNumeroEntero;
